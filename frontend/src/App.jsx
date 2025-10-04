ef6130720a27205a3de93c00a3f4fe783d415ab0
gyan011 <gyanranjan9661@gmail.com>
2025-10-04 13:55:46 +0530

    manger

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import {Routes, Route} from "react-router-dom"
 import  Home  from "./components/Home"
 import RegisterPage from "./pages/Register";
 import LoginPage from "./pages/Login";
+import ManagerDashboard from "./pages/ManagerDashboard";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/manager" element={ <ManagerDashboard /> } />
       </Routes>
     </div>
   );
